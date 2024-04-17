4dbcb654c74f69d1610347bd0c83b1c96d6d02a6
GabrielGaribaldi <166620362+GabrielGaribaldi@users.noreply.github.com>
2024-04-17 10:33:17 -0300

    Atualização na declaração das variáveis

--- aula-06-exercicios/exercicio-2/index.js
+++ aula-06-exercicios/exercicio-2/index.js
@@ -3,9 +3,9 @@ const pergunta1 = "Você gosta de pizza?"
 const pergunta2 = "Você possui algum animal de estimação?"
 const pergunta3 = "Você já bebeu água hoje?"
 
-let resposta1 = prompt(pergunta1)
-let resposta2 = prompt(pergunta2)
-let resposta3 = prompt(pergunta3)
+const resposta1 = prompt(pergunta1)
+const resposta2 = prompt(pergunta2)
+const resposta3 = prompt(pergunta3)
 
 
 console.log(pergunta1, "- R:", resposta1)
